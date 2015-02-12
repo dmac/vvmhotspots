dd8943c1ce052d0b01f53f0bbbe564c61f73dfa3
Daniel MacDougall <dmacdougall@gmail.com>
2015-02-11 18:53:47 -0800

    Back out tree view experiment

--- main.go
+++ main.go
@@ -11,26 +11,13 @@ import (
 )
 
 type Node struct {
-	Name       string
-	TimeCPU    int `xml:"Time-CPU"`
-	RelTimeCPU float32
-	Nodes      []*Node `xml:"node"`
-	Depth      int
+	Name    string
+	Parent  string
+	TimeCPU int     `xml:"Time-CPU"`
+	Nodes   []*Node `xml:"node"`
 }
 
-func (n *Node) ComputeRelTimeCPU(totalTimeCPU int) {
-	n.RelTimeCPU = float32(n.TimeCPU) / float32(totalTimeCPU)
-}
-
-func (n *Node) ComputeRelTimeCPURecursive(totalTimeCPU, depth int) {
-	n.ComputeRelTimeCPU(totalTimeCPU)
-	n.Depth = depth
-	for _, node := range n.Nodes {
-		node.ComputeRelTimeCPURecursive(totalTimeCPU, depth+1)
-	}
-}
-
-type NameTimePair struct {
+type NameRelTimeCPU struct {
 	Name       string
 	RelTimeCPU float32
 }
@@ -44,39 +31,18 @@ func main() {
 		Tree Node `xml:"tree"`
 	}{}
 	xml.Unmarshal(data, &v)
+
 	root := v.Tree.Nodes[0]
-
-	root.ComputeRelTimeCPURecursive(root.TimeCPU, 0)
-
-	nodes := []*Node{}
-	dft := []*Node{root}
-	var node *Node
-	for len(dft) > 0 {
-		node, dft = dft[len(dft)-1], dft[:len(dft)-1]
-		sort.Sort(ByRelTimeCPU(node.Nodes))
-		nodes = append(nodes, node)
-		dft = append(dft, node.Nodes...)
+	nodes := []*Node{root}
+	for i := 0; i < len(nodes); i++ {
+		nodes = append(nodes, nodes[i].Nodes...)
 	}
-
-	//for _, node := range nodes {
-	//if strings.hasprefix(node.name, "clojure.lang") ||
-	//strings.hasprefix(node.name, "clojure.core") ||
-	//node.name == "self time" {
-	//continue
-	//}
-	//for i := 0; i < node.Depth; i++ {
-	//fmt.Print("  ")
-	//}
-	//fmt.Println(node.Name, node.RelTimeCPU)
-	//}
-
-	//os.Exit(0)
 
 	nodesTimeCPU := map[string]int{}
 	for _, node := range nodes {
-		if strings.HasPrefix(node.Name, "clojure.lang") ||
-			strings.HasPrefix(node.Name, "clojure.core") ||
-			node.Name == "Self time" {
+		// TODO(dmac) This prevents overloaded functions from being double-counted. But, does it also
+		// mask time spent in recursive functions?
+		if node.Name == node.Parent {
 			continue
 		}
 		if _, found := nodesTimeCPU[node.Name]; !found {
@@ -85,41 +51,34 @@ func main() {
 		nodesTimeCPU[node.Name] += node.TimeCPU
 	}
 
-	//totalTimeCPU := v.Tree.Nodes[0].TimeCPU
-	//pairs := []NameTimePair{}
-	//for name, timeCPU := range nodesTimeCPU {
-	//pair := NameTimePair{name, float32(timeCPU) / float32(totalTimeCPU)}
-	//pairs = append(pairs, pair)
-	//}
+	pairs := []NameRelTimeCPU{}
+	for name, timeCPU := range nodesTimeCPU {
+		pair := NameRelTimeCPU{name, float32(timeCPU) / float32(root.TimeCPU)}
+		pairs = append(pairs, pair)
+	}
 
-	//sort.Sort(sort.Reverse(ByRelTimeCPU(pairs)))
-
-	sort.Sort(sort.Reverse(ByRelTimeCPU(nodes)))
+	sort.Sort(sort.Reverse(ByRelTimeCPU(pairs)))
 
 	w := new(tabwriter.Writer)
 	w.Init(os.Stdout, 0, 8, 1, ' ', 0)
-
 	nPrinted := 0
-	for _, node := range nodes {
-		if strings.HasPrefix(node.Name, "clojure.lang") ||
-			strings.HasPrefix(node.Name, "clojure.core") ||
-			node.Name == "self time" {
+	nToPrint := 50
+	for _, pair := range pairs {
+		if strings.HasPrefix(pair.Name, "clojure.lang") ||
+			strings.HasPrefix(pair.Name, "clojure.core") ||
+			pair.Name == "Self time" {
 			continue
 		}
-		fmt.Fprintf(w, "%s\t%f\n", node.Name, node.RelTimeCPU)
+		fmt.Fprintf(w, "%s\t%f\n", pair.Name, pair.RelTimeCPU)
 		nPrinted++
-		if nPrinted >= 100 {
+		if nPrinted >= nToPrint {
 			break
 		}
 	}
 	w.Flush()
-
-	pct := float32(nodesTimeCPU["ml_lib.geo_ip$ip_to_location.invoke()"]) /
-		float32(nodesTimeCPU["haggler.handler$bid.invoke()"])
-	fmt.Println(pct)
 }
 
-type ByRelTimeCPU []*Node
+type ByRelTimeCPU []NameRelTimeCPU
 
 func (a ByRelTimeCPU) Len() int           { return len(a) }
 func (a ByRelTimeCPU) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
