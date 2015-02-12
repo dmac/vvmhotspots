3861a5ce1071e54b679945cd7cf866fe34024fa3
Daniel MacDougall <dmacdougall@gmail.com>
2015-02-11 18:34:48 -0800

    Experiment with outputting tree view

--- main.go
+++ main.go
@@ -15,16 +15,18 @@ type Node struct {
 	TimeCPU    int `xml:"Time-CPU"`
 	RelTimeCPU float32
 	Nodes      []*Node `xml:"node"`
+	Depth      int
 }
 
 func (n *Node) ComputeRelTimeCPU(totalTimeCPU int) {
 	n.RelTimeCPU = float32(n.TimeCPU) / float32(totalTimeCPU)
 }
 
-func (n *Node) ComputeRelTimeCPURecursive(totalTimeCPU int) {
+func (n *Node) ComputeRelTimeCPURecursive(totalTimeCPU, depth int) {
 	n.ComputeRelTimeCPU(totalTimeCPU)
+	n.Depth = depth
 	for _, node := range n.Nodes {
-		node.ComputeRelTimeCPURecursive(totalTimeCPU)
+		node.ComputeRelTimeCPURecursive(totalTimeCPU, depth+1)
 	}
 }
 
@@ -44,16 +46,31 @@ func main() {
 	xml.Unmarshal(data, &v)
 	root := v.Tree.Nodes[0]
 
-	root.ComputeRelTimeCPURecursive(root.TimeCPU)
+	root.ComputeRelTimeCPURecursive(root.TimeCPU, 0)
 
-	os.Exit(0)
-
-	nodes := []*Node{v.Tree.Nodes[0]}
-	i := 0
-	for i < len(nodes) {
-		nodes = append(nodes, nodes[i].Nodes...)
-		i++
+	nodes := []*Node{}
+	dft := []*Node{root}
+	var node *Node
+	for len(dft) > 0 {
+		node, dft = dft[len(dft)-1], dft[:len(dft)-1]
+		sort.Sort(ByRelTimeCPU(node.Nodes))
+		nodes = append(nodes, node)
+		dft = append(dft, node.Nodes...)
 	}
+
+	//for _, node := range nodes {
+	//if strings.hasprefix(node.name, "clojure.lang") ||
+	//strings.hasprefix(node.name, "clojure.core") ||
+	//node.name == "self time" {
+	//continue
+	//}
+	//for i := 0; i < node.Depth; i++ {
+	//fmt.Print("  ")
+	//}
+	//fmt.Println(node.Name, node.RelTimeCPU)
+	//}
+
+	//os.Exit(0)
 
 	nodesTimeCPU := map[string]int{}
 	for _, node := range nodes {
@@ -68,19 +85,32 @@ func main() {
 		nodesTimeCPU[node.Name] += node.TimeCPU
 	}
 
-	totalTimeCPU := v.Tree.Nodes[0].TimeCPU
-	pairs := []NameTimePair{}
-	for name, timeCPU := range nodesTimeCPU {
-		pair := NameTimePair{name, float32(timeCPU) / float32(totalTimeCPU)}
-		pairs = append(pairs, pair)
-	}
-	sort.Sort(sort.Reverse(ByRelTimeCPU(pairs)))
+	//totalTimeCPU := v.Tree.Nodes[0].TimeCPU
+	//pairs := []NameTimePair{}
+	//for name, timeCPU := range nodesTimeCPU {
+	//pair := NameTimePair{name, float32(timeCPU) / float32(totalTimeCPU)}
+	//pairs = append(pairs, pair)
+	//}
+
+	//sort.Sort(sort.Reverse(ByRelTimeCPU(pairs)))
+
+	sort.Sort(sort.Reverse(ByRelTimeCPU(nodes)))
 
 	w := new(tabwriter.Writer)
 	w.Init(os.Stdout, 0, 8, 1, ' ', 0)
 
-	for i := 0; i < 100; i++ {
-		fmt.Fprintf(w, "%s\t%f\n", pairs[i].Name, pairs[i].RelTimeCPU)
+	nPrinted := 0
+	for _, node := range nodes {
+		if strings.HasPrefix(node.Name, "clojure.lang") ||
+			strings.HasPrefix(node.Name, "clojure.core") ||
+			node.Name == "self time" {
+			continue
+		}
+		fmt.Fprintf(w, "%s\t%f\n", node.Name, node.RelTimeCPU)
+		nPrinted++
+		if nPrinted >= 100 {
+			break
+		}
 	}
 	w.Flush()
 
@@ -89,7 +119,7 @@ func main() {
 	fmt.Println(pct)
 }
 
-type ByRelTimeCPU []NameTimePair
+type ByRelTimeCPU []*Node
 
 func (a ByRelTimeCPU) Len() int           { return len(a) }
 func (a ByRelTimeCPU) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
